aea99de1738d78d3ba210c062e2801b2a9965e3a
Poltergeist <spectator.networx@gmail.com>
2015-01-08 15:18:44 +0100

    feat(example) clear database before adding messages.

--- example/basic.js
+++ example/basic.js
@@ -1,39 +1,44 @@
-var relaxLog = require('../lib/index')({url: 'http://localhost:5984/relaxlog'});
+var relaxLog = require('../lib/index')({url: 'http://localhost:5984/relaxlogexample'}),
+  nano = require('nano')('http://localhost:5984');
 
-relaxLog.info('Starting to Log');
+nano.db.destroy('relaxlogexample', function() {
+  nano.db.create('relaxlogexample', function() {
+    relaxLog.info('Starting to Log');
 
-relaxLog.error('Miserable Failure occured');
+    relaxLog.error('Miserable Failure occured');
 
-relaxLog.info('Restarting task');
+    relaxLog.info('Restarting task');
 
-relaxLog.error('Miserable Failure occured again');
-setTimeout(function() {
-  relaxLog.info('Email send to Admin');
-}, 500);
-relaxLog.info(
-  {
-    message: 'Starting to Log',
-    custom: true
-  }
-);
+    relaxLog.error('Miserable Failure occured again');
+    setTimeout(function() {
+      relaxLog.info('Email send to Admin');
+    }, 500);
+    relaxLog.info(
+      {
+        message: 'Starting to Log',
+        custom: true
+      }
+    );
 
-relaxLog.error(
-  {
-    message: 'Miserable Failure occured',
-    custom: true
-  }
-);
+    relaxLog.error(
+      {
+        message: 'Miserable Failure occured',
+        custom: true
+      }
+    );
 
-relaxLog.info(
-  {
-    message: 'Restarting task',
-    custom: true
-  }
-);
+    relaxLog.info(
+      {
+        message: 'Restarting task',
+        custom: true
+      }
+    );
 
-relaxLog.error(
-  {
-    message: 'Miserable Failure occured again',
-    custom: true
-  }
-);
+    relaxLog.error(
+      {
+        message: 'Miserable Failure occured again',
+        custom: true
+      }
+    );
+  });
+});
